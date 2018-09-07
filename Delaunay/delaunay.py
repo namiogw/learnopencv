15d79ba4e88f1a7df61425bb36091fe85261f7f5
namiogw <namiogw@gmail.com>
2018-09-07 18:28:58 +0900

    working

--- Delaunay/delaunay.py
+++ Delaunay/delaunay.py
@@ -18,7 +18,7 @@ def rect_contains(rect, point) :
 
 # Draw a point
 def draw_point(img, p, color ) :
-    cv2.circle( img, p, 2, color, cv2.cv.CV_FILLED, cv2.CV_AA, 0 )
+    cv2.circle( img, p, 2, color, cv2.FILLED, CV_AA, 0 )
 
 
 # Draw delaunay triangles
@@ -29,16 +29,16 @@ def draw_delaunay(img, subdiv, delaunay_color ) :
     r = (0, 0, size[1], size[0])
 
     for t in triangleList :
-        
+
         pt1 = (t[0], t[1])
         pt2 = (t[2], t[3])
         pt3 = (t[4], t[5])
-        
+
         if rect_contains(r, pt1) and rect_contains(r, pt2) and rect_contains(r, pt3) :
-        
-            cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.CV_AA, 0)
-            cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.CV_AA, 0)
-            cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.CV_AA, 0)
+
+            cv2.line(img, pt1, pt2, delaunay_color, 1, CV_AA, 0)
+            cv2.line(img, pt2, pt3, delaunay_color, 1, CV_AA, 0)
+            cv2.line(img, pt3, pt1, delaunay_color, 1, CV_AA, 0)
 
 
 # Draw voronoi diagram
@@ -46,22 +46,23 @@ def draw_voronoi(img, subdiv) :
 
     ( facets, centers) = subdiv.getVoronoiFacetList([])
 
-    for i in xrange(0,len(facets)) :
+    for i in range(0,len(facets)) :
         ifacet_arr = []
         for f in facets[i] :
             ifacet_arr.append(f)
-        
+
         ifacet = np.array(ifacet_arr, np.int)
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-        cv2.fillConvexPoly(img, ifacet, color, cv2.CV_AA, 0);
+        cv2.fillConvexPoly(img, ifacet, color, CV_AA, 0);
         ifacets = np.array([ifacet])
-        cv2.polylines(img, ifacets, True, (0, 0, 0), 1, cv2.CV_AA, 0)
-        cv2.circle(img, (centers[i][0], centers[i][1]), 3, (0, 0, 0), cv2.cv.CV_FILLED, cv2.CV_AA, 0)
+        cv2.polylines(img, ifacets, True, (0, 0, 0), 1, CV_AA, 0)
+        cv2.circle(img, (centers[i][0], centers[i][1]), 3, (0, 0, 0), cv2.FILLED, CV_AA, 0)
 
 
 if __name__ == '__main__':
 
+    CV_AA = 16
     # Define window names
     win_delaunay = "Delaunay Triangulation"
     win_voronoi = "Voronoi Diagram"
